2cd6dded3ef5530eb553852252f82af4b300b47b
Ben Neely <ben@uceem.com>
2013-09-30 15:53:38 -0600

    Redirect index

--- index.php
+++ index.php
@@ -1,1 +1,11 @@
-It works!
+<!DOCTYPE <!DOCTYPE html>
+<html>
+<head>
+  <meta http-equiv="refresh" content="0; url=http://cricketandbea.herokuapp.com/project/index.php/">
+  <title></title>
+</head>
+<body>
+
+</body>
+</html>
+</DIV>
