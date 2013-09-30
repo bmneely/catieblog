75bd64db37021d6e5be8b4a14dd7bef1b07f0d33
Ben Neely <ben@uceem.com>
2013-09-30 16:03:10 -0600

    More stuff

--- project/partials/banner.php
+++ project/partials/banner.php
@@ -1,6 +1,6 @@
 <div id="header">
   <div>
-    <a href="/project/index.php"><img id="logo" src="/project/assets/images/logo.jpg" alt="cricket and bea"></a>
+    <a href="index.php"><img id="logo" src="assets/images/logo.jpg" alt="cricket and bea"></a>
     <div class="banner"></div>
   </div>
 
@@ -9,7 +9,7 @@
       <li class="<?php print $page === 'blog' ? 'active' : 'inactive' ?>"><a href="/project/index.php">Home</a></li>
       <li class="<?php print $page === 'about' ? 'active' : 'inactive' ?>"><a href="/project/about.php">About</a></li>
       <li class="<?php print $page === 'gallery' ? 'active' : 'inactive' ?>"><a href="/project/gallery.php">Gallery</a></li>
-      <li class="<?php print $page === 'store' ? 'active' : 'inactive' ?>"><a href="/project/store/inventory.php">Store</a></li>
+      <li class="<?php print $page === 'store' ? 'active' : 'inactive' ?>"><a href="/project/store.php">Store</a></li>
     </ul>
   </div>
 </div>
