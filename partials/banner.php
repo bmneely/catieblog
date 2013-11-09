e984d2a9cf81c60a0c446642e935e761e9c92943
Ben Neely <benjaminneely@u.boisestate.edu>
2013-11-09 14:09:39 -0700

    Sign in

--- partials/banner.php
+++ partials/banner.php
@@ -10,7 +10,7 @@
       <li class="<?php print $page === 'about' ? 'active' : 'inactive' ?>"><a href="/about.php">About</a></li>
       <li class="<?php print $page === 'gallery' ? 'active' : 'inactive' ?>"><a href="/gallery.php">Gallery</a></li>
       <li class="<?php print $page === 'store' ? 'active' : 'inactive' ?>"><a href="/store.php">Store</a></li>
+      <li><i class="fa fa-sign-in float-right"></i> Sign In</li>
     </ul>
-    <i class="fa fa-sign-in"></i> Sign In
   </div>
 </div>
