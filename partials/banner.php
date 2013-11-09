1df7d1a9c0fd3bd4193a67bb60a03b90b79b8829
Ben Neely <benjaminneely@u.boisestate.edu>
2013-11-09 15:30:21 -0700

    Sign in

--- partials/banner.php
+++ partials/banner.php
@@ -15,8 +15,8 @@
   </div>
 </div>
 
-<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js">
-  
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+<script>
   alert("hlelo");
   $("float-right").append("ljalsdjfljlfdsajlfsdajdflasdsafljjlafsdljafsjladfsjlkafsd");
 </script>
