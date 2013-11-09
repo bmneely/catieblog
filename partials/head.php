2ecc39bd84d53d9edd878279497a4e340f6f4ef4
Ben Neely <benjaminneely@u.boisestate.edu>
2013-11-09 13:57:11 -0700

    Sign-in icon

--- partials/head.php
+++ partials/head.php
@@ -4,5 +4,6 @@
   <link href="/assets/stylesheets/banner-nav.css" rel="stylesheet" type="text/css">
   <link href="/assets/stylesheets/content.css" rel="stylesheet" type="text/css">
   <link href="/assets/stylesheets/footer.css" rel="stylesheet" type="text/css">
+  <link href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
   <link rel="icon" type="image/gif" href="/assets/images/favicon.gif" />
 </head>
