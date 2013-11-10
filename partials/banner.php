d8f0ef3681fad64dd635614d51cbb786a05c5e92
Ben Neely <benjaminneely@u.boisestate.edu>
2013-11-09 20:21:03 -0700

    Sign in

--- partials/banner.php
+++ partials/banner.php
@@ -10,7 +10,7 @@
       <li class="<?php print $page === 'about' ? 'active' : 'inactive' ?>"><a href="/about.php">About</a></li>
       <li class="<?php print $page === 'gallery' ? 'active' : 'inactive' ?>"><a href="/gallery.php">Gallery</a></li>
       <li class="<?php print $page === 'store' ? 'active' : 'inactive' ?>"><a href="/store.php">Store</a></li>
-      <li class="float-right"><a href="#" onclick="toggleSignIn()"><i class="fa fa-sign-in"></i> Log In</a></li>
+      <li class="float-right"><a href="" onclick="toggleSignIn()"><i class="fa fa-sign-in"></i> Log In</a></li>
     </ul>
   </div>
   <div class="sign-in">
@@ -25,6 +25,9 @@
     </div>
   </div>
   <div class="clear"></div>
+  <div class="sign-up">
+
+  </div>
 </div>
 
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
