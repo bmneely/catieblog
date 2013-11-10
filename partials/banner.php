135234ac4a88651881a49133988762318f7beb48
Ben Neely <benjaminneely@u.boisestate.edu>
2013-11-09 21:17:57 -0700

    Sign in

--- partials/banner.php
+++ partials/banner.php
@@ -10,17 +10,17 @@
       <li class="<?php print $page === 'about' ? 'active' : 'inactive' ?>"><a href="/about.php">About</a></li>
       <li class="<?php print $page === 'gallery' ? 'active' : 'inactive' ?>"><a href="/gallery.php">Gallery</a></li>
       <li class="<?php print $page === 'store' ? 'active' : 'inactive' ?>"><a href="/store.php">Store</a></li>
-      <li class="float-right"><div="hand-pointer" onclick="toggleSignIn()"><i class="fa fa-sign-in"></i> Log In</div></li>
+      <li class="float-right"><a href="#" onclick="toggleSignIn()"><i class="fa fa-sign-in"></i> Log In</a></li>
     </ul>
   </div>
   <div class="sign-in">
     <div>
-      <div class="hand-pointer sign-in-form close-icon" onclick="toggleSignIn()">&#10006;</div>
+      <a class="sign-in-form close-icon" href="#" onclick="toggleSignIn()">&#10006;</a>
       <br>
       <form class="sign-in-form" action="session/signin.php" method="post">
         <label>Email:<br><input type="text" name="email" /></label><br/>
         <label>Pasword:<br><input type="password" name="password" /></label>
-        <input class="submit" type="submit" value="Log In" />/<span class='sign-in-form sign-in-link'><div="hand-pointer" onclick="toggleSignUp()">Sign Up</div></span>
+        <input class="submit" type="submit" value="Log In" />/<span class='sign-in-form sign-in-link'><<a href="#" onclick="toggleSignUp()">Sign Up</a></span>
       </form>
     </div>
   </div>
