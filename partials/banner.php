5f32e30d211d73a458b9107d281f7e92ae58f7bb
Ben Neely <benjaminneely@u.boisestate.edu>
2013-11-10 14:23:18 -0700

    Sign up

--- partials/banner.php
+++ partials/banner.php
@@ -10,7 +10,8 @@
       <li class="<?php print $page === 'about' ? 'active' : 'inactive' ?>"><a href="/about.php">About</a></li>
       <li class="<?php print $page === 'gallery' ? 'active' : 'inactive' ?>"><a href="/gallery.php">Gallery</a></li>
       <li class="<?php print $page === 'store' ? 'active' : 'inactive' ?>"><a href="/store.php">Store</a></li>
-      <?php if(isset($_SESSION["name"])){
+      <?php if(isset($_SESSION["name"]))
+      // {
         //   echo "<li class='float-right'><a href='#'' onclick="toggleSignIn()"><i class='"'fa fa-sign-out'"'></i><span class='"'log-in-text'"'>" . $_SESSION["name"] ."</span></a></li>"
         // } else {
         //   echo "<li class='float-right'><a href='#' onclick='toggleSignIn()'><i class='fa fa-sign-in'></i><span class='log-in-text'>Log In</span></a></li>"
