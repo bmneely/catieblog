175d95a1114c813403b89ba6e8940ffa448ad52e
Ben Neely <benjaminneely@u.boisestate.edu>
2013-11-09 20:16:02 -0700

    Sign in

---
+++ session/signup.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+  <?php include("../partials/head.php") ?>
+  <link href="/assets/stylesheets/store-styles.css" rel="stylesheet" type="text/css">
+<body>
+  <div id="container">
+
+    <?php include("../partials/banner.php") ?>
+
+    <div id="body">
+
+
+
+
+    </div>
+    <?php include("../partials/footer.php") ?>
+  </div>
+</html>
