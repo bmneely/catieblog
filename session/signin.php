abc07a583f9a6089b7185f2afbb34d9d538e8ae5
Ben Neely <benjaminneely@u.boisestate.edu>
2013-11-10 14:45:00 -0700

    Sign up

--- session/signin.php
+++ session/signin.php
@@ -1,8 +1,7 @@
 <?php
   require_once "../resources/Dao.php";
 
-  // $dao = new Dao();
-
+  $dao = new Dao();
 
 	$email = "";
 	$pass = "";
@@ -18,34 +17,34 @@
 	}
 
 
-	// if ($email !== "" && $pass !== ""){
-	// 	if(is_password_correct($email, $pass)){
-	// 		$user = $dao->getUser($email);
-	// 		session_start();
-	// 		$_SESSION["name"] = user_name($user);
-	// 	}
-	// }
+	if ($email !== "" && $pass !== ""){
+		if(is_password_correct($email, $pass)){
+			$user = $dao->getUser($email);
+			session_start();
+			$_SESSION["name"] = user_name($user);
+		}
+	}
 
- //   function is_password_correct($email, $password){
- //   		$user = $dao->getUser($email);
- //   		if ($user["password"] == $password){
- //   			return TRUE;
- //   		}
- //   		return FALSE;
- //   }
+   function is_password_correct($email, $password){
+   		$user = $dao->getUser($email);
+   		if ($user["password"] == $password){
+   			return TRUE;
+   		}
+   		return FALSE;
+   }
 
- //  function user_name($user){
- //  	$user_name = ""
- //  	if ($user["first_name"] == NULL || $user["first_name"] == ""){
- //  		if ($user["last_name"] == NULL || $user["last_name"] == ""){
- //  			$user_name = $user["email"];
- //  		} else {
- //  			$user_name = ucwords($user["last_name"]);
- //  		}
- //  	}
- //  	else {
- //  		$user_name = ucwords($user["first_name"] . " " . $user["last_name"]);
- //  	}
- //  	return $user_name;
- //  }
+  function user_name($user){
+  	$user_name = ""
+  	if ($user["first_name"] == NULL || $user["first_name"] == ""){
+  		if ($user["last_name"] == NULL || $user["last_name"] == ""){
+  			$user_name = $user["email"];
+  		} else {
+  			$user_name = ucwords($user["last_name"]);
+  		}
+  	}
+  	else {
+  		$user_name = ucwords($user["first_name"] . " " . $user["last_name"]);
+  	}
+  	return $user_name;
+  }
 ?>
