a4f41526ee44076f5b34a657c088560c770149c3
Ben Neely <benjaminneely@u.boisestate.edu>
2013-11-09 21:32:41 -0700

    Sign in

--- session/signin.php
+++ session/signin.php
@@ -1,13 +1,13 @@
 <?php
 
 
-	// $user="";
-	// $pass="";
-	// if(isset($_POST['email'])){$time=$_POST['eamil']}
-	// if(isset($_POST['pasword'])){$time=$_POST['password']}
+	$user="";
+	$pass="";
+	if(isset($_POST['email'])){$time=$_POST['email']}
+	if(isset($_POST['pasword'])){$time=$_POST['password']}
 
-	// echo $user."<br>";
-	// echo $pass;
+	echo $user."<br>";
+	echo $pass;
 ?>
 
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
