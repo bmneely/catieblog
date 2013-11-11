9fd1ba32f402339be1f812f8f78cb0792c56d531
Ben Neely <benjaminneely@u.boisestate.edu>
2013-11-10 18:27:27 -0700

    Sign up

--- session/signin.php
+++ session/signin.php
@@ -3,51 +3,53 @@
 
   $dao = new Dao();
 
-  function user_name($user){
-  	$user_name = ""
-  	if (is_null($user["first_name"]) || $user["first_name"] === ""){
-  		if (is_null($user["last_name"])|| $user["last_name"] === ""){
-  			$user_name = $user["email"];
-  		} else {
-  			$user_name = ucwords($user["last_name"]);
-  		}
-  	}
-  	else {
-  		$user_name = ucwords($user["first_name"] . " " . $user["last_name"]);
-  	}
-  	return $user_name;
-  }
-  
-  $email = "";
-  $pass = "";
+	$email = "";
+	$pass = "";
 
-  if(isset($_POST['email']))
-  {
-    $email = $_POST['email'];
-  }
+	if(isset($_POST['email']))
+	{
+		$email = $_POST['email'];
+	}
 
-  if(isset($_POST['password']))
-  {
-    $pass = $_POST['password'];
-  }
+	if(isset($_POST['password']))
+	{
+		$pass = $_POST['password'];
+	}
 
 
-  if ($email !== "" && $pass !== ""){
-    // if(is_password_correct($email, $pass)){
-      $user = $dao->getUser($email);
-      session_start();
-      $_SESSION["name"] = user_name($user);
+	if ($email !== "" && $pass !== ""){
+		// if(is_password_correct($email, $pass)){
+			$user = $dao->getUser($email);
+			session_start();
+			$_SESSION["name"] = user_name($user);
 
-    // }
-  }
+		// }
+	}
 
   // function is_password_correct($email, $password){
-  //  $user = $dao->getUser($email);
-  //  if ($user["password"] === $password){
-  //    return TRUE;
-  //  }
-  //  return FALSE;
+ 	// 	$user = $dao->getUser($email);
+ 	// 	if ($user["password"] === $password){
+ 	// 		return TRUE;
+ 	// 	}
+ 	// 	return FALSE;
   // }
+
+  function user_name($user){
+    echo $user;
+  	// $user_name = ""
+  	// if (is_null($user["first_name"]) || $user["first_name"] === ""){
+  	// 	if (is_null($user["last_name"])|| $user["last_name"] === ""){
+  	// 		$user_name = $user["email"];
+  	// 	} else {
+  	// 		$user_name = ucwords($user["last_name"]);
+  	// 	}
+  	// }
+  	// else {
+  	// 	$user_name = ucwords($user["first_name"] . " " . $user["last_name"]);
+  	// }
+  	// return $user_name;
+  }
+
 
 
   echo "HELOOOOO   ";
