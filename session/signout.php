017a387fa77221dcdb94c0a65c6aa681293c8270
Ben Neely <benjaminneely@u.boisestate.edu>
2013-11-10 19:34:51 -0700

    Sign up

---
+++ session/signout.php
@@ -0,0 +1,16 @@
+<?php
+  session_start();
+  unset($_SESSION["name"]);
+  session_destroy();
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="refresh" content="0; url=http://www.cricketandbea.com/" />
+</head>
+<body>
+  
+</body>
+</html>
