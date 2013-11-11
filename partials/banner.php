88e12541561785d64a28da98764c58fcfbc059b2
Ben Neely <benjaminneely@u.boisestate.edu>
2013-11-10 19:23:17 -0700

    Sign up

--- partials/banner.php
+++ partials/banner.php
@@ -73,7 +73,8 @@
         data: $('form').serialize(),
         success: function () {
           $("#login-area").empty();
-          user_name = '<?php echo $_SESSION["name"]; ?>';
+
+          user_name = '<?php session_start(); echo $_SESSION["name"]; ?>';
           console.log(user_name);
           $("#login-area").append("<li class='float-right'><a href='#' onclick='toggleSignIn()'><span class='log-in-text'>" + user_name + "</span><i class='fa fa-sign-out'></i></a></li>");
         }
