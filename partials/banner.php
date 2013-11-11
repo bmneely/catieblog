fc94a710bc8c84fadbb554d0a1991f55dae371a7
Ben Neely <benjaminneely@u.boisestate.edu>
2013-11-10 17:16:41 -0700

    Sign up

--- partials/banner.php
+++ partials/banner.php
@@ -11,11 +11,18 @@
       <li class="<?php print $page === 'gallery' ? 'active' : 'inactive' ?>"><a href="/gallery.php">Gallery</a></li>
       <li class="<?php print $page === 'store' ? 'active' : 'inactive' ?>"><a href="/store.php">Store</a></li>
       <span id="login-area">
-        <?php if(isset($_SESSION["name"]))
+        <?php
+
+         session_start();
+         $_SESSION['user'] = "Ben Neely"
+
+        if(isset($_SESSION["name"]))
           {
             echo "<li class='float-right'><a href='#' onclick='toggleSignIn()'><span class='log-in-text'>" . $_SESSION["name"] ."</span><i class='fa fa-sign-out'></i></a></li>";
           } else {
-            echo "<li class='float-right'><a href='#' onclick='toggleSignIn()'><span class='log-in-text'>Log In</span><i class='fa fa-sign-in'></i></a></li>";
+            ?>
+            <li class='float-right'><a href='#' onclick='toggleSignIn()'><span class='log-in-text'>Log In</span><i class='fa fa-sign-in'></i></a></li>";
+            <?php
           }
         ?>
       </span>
