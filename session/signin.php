fa426dd59f54a1a57cc3a17b9d5ec0df0b3f4a74
Ben Neely <benjaminneely@u.boisestate.edu>
2013-11-10 18:32:23 -0700

    Sign up

--- session/signin.php
+++ session/signin.php
@@ -35,20 +35,18 @@
   // }
 
   function user_name($user){
-    // echo $user;
-    echo $user["first_name"];
-  	$user_name = ""
-  	// if (is_null($user["first_name"]) || $user["first_name"] === ""){
-  	// 	// if (is_null($user["last_name"])|| $user["last_name"] === ""){
-  	// 	// 	$user_name = $user["email"];
-  	// 	// } else {
-  	// 	// 	$user_name = ucwords($user["last_name"]);
-  	// 	// }
-  	// }
-  	// // else {
-  	// // 	$user_name = ucwords($user["first_name"] . " " . $user["last_name"]);
-  	// // }
-  	// return $user_name;
+  	$user_name = "";
+  	if (is_null($user["first_name"]) || $user["first_name"] === ""){
+  		if (is_null($user["last_name"])|| $user["last_name"] === ""){
+  			$user_name = $user["email"];
+  		} else {
+  			$user_name = ucwords($user["last_name"]);
+  		}
+  	}
+  	else {
+  		$user_name = ucwords($user["first_name"] . " " . $user["last_name"]);
+  	}
+  	return $user_name;
   }
 
 
