df59c2079b214f76b5f671fc67f2f8436fc7252a
Ben Neely <benjaminneely@u.boisestate.edu>
2013-11-11 18:59:28 -0700

    Sign up

--- session/signup.php
+++ session/signup.php
@@ -8,7 +8,8 @@
 	$email = $_POST['email'];
 	$first = $_POST['first'];
 	$last = $_POST['last'];
-	$hashed_password = $pass->crypt_pass($_POST['password']);
+	$hashed_password = "lajfldsjl"
+	// $hashed_password = $pass->crypt_pass($_POST['password']);
 	$role = "USER";
 
 	$dao->saveUser($first, $last, $email, $hashed_password, $role);
