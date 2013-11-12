da1e171632ce8dec370d5130c5b1e0af1654012e
Ben Neely <benjaminneely@u.boisestate.edu>
2013-11-11 18:52:43 -0700

    Sign up

---
+++ resources/Pass.php
@@ -0,0 +1,18 @@
+<?php
+
+class Pass {
+
+  private $salt = '@#soRand!&)**()'
+
+  function is_password_correct($user, $password){
+    if ($user["password"] === $password){
+      return TRUE;
+    } else {
+      return FALSE;
+    }
+  }
+
+  function crypt_pass($plain_pass){
+    return crypt($plain_pass, $salt);
+  }
+}
