d6959b17bc002dfd463c6e4ec2e0feed8fbd60ac
Ben Neely <benjaminneely@u.boisestate.edu>
2013-11-17 10:52:52 -0700

    Sign in

--- partials/banner.php
+++ partials/banner.php
@@ -39,7 +39,7 @@
   <div class="sign-up">
       <a class="close-icon" href="#" onclick="toggleSignUp()">&#10006;</a>
       <br>
-      <form class="sign-un-form" action="../session/signup.php" method="post">
+      <form class="sign-up-form" action="../session/signup.php" method="post">
         <label><span class="sign-up-label">First Name</span><input type="text" name="first" /></label><br/>
         <label><span class="sign-up-label">Last Name</span><input type="text" name="last" /></label><br/>
         <label><span class="sign-up-label">Email</span><input type="text" name="email" /></label><br/>
