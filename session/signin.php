fb2e2bf9b1d79261d9d7445ea83ed3370b23eb7e
Ben Neely <benjaminneely@u.boisestate.edu>
2013-11-20 12:53:36 -0700

    Working on forms

--- session/signin.php
+++ session/signin.php
@@ -14,10 +14,10 @@
     $_SESSION["email_error"] = "An email is required";
   } else {
     $email = test_input($_POST["email"])
-    if (!preg_match("/([\w\-]+\@[\w\-]+\.[\w\-]+)/",$email)) {
-      $_SESSION["form_errors"] = true;
-      $_SESSION["email_error"] = "Your email is invalid";
-    } 
+    // if (!preg_match("/([\w\-]+\@[\w\-]+\.[\w\-]+)/",$email)) {
+    //   $_SESSION["form_errors"] = true;
+    //   $_SESSION["email_error"] = "Your email is invalid";
+    // } 
   }
 
   // if(empty($_POST["pasword"])) {
