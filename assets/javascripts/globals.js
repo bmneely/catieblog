a3456afe8786a3157062383c1cc442ec244b39aa
Ben Neely <benjaminneely@u.boisestate.edu>
2013-11-19 22:22:20 -0700

    Code cleanup

--- assets/javascripts/globals.js
+++ assets/javascripts/globals.js
@@ -9,3 +9,21 @@
   function toggleSignUp(){
     $(".sign-up").toggle();
   }
+
+  // $(function () {
+  //   $('form').on('submit', function (e) {
+  //     $.ajax({
+  //       type: 'post',
+  //       url: '/session/signin.php',
+  //       data: $('form').serialize(),
+  //       success: function () {
+  //         $("#login-area").empty();
+
+  //         user_name = '<?php session_start(); echo $_SESSION["name"]; ?>';
+  //         console.log(user_name);
+  //         $("#login-area").append("<li class='float-right'><a href='#' onclick='toggleSignIn()'><span class='log-in-text'>" + user_name + "</span><i class='fa fa-sign-out'></i></a></li>");
+  //       }
+  //     });
+  //     e.preventDefault();
+  //   });
+  // });
--- partials/banner.php
+++ partials/banner.php
@@ -49,39 +49,4 @@
   </div>
   <div class="clear"></div>
 </div>
-  <div class="clear"></div>
-<!-- 
-<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
-<script>
-  function toggleSignIn(){
-    if ($(".sign-up").is(":visible")){
-      $(".sign-up").toggle();
-    } else {
-      $(".sign-in").toggle();   
-    }
-  }
-
-  function toggleSignUp(){
-    $(".sign-up").toggle();
-  }
-
-  // $(function () {
-  //   $('form').on('submit', function (e) {
-  //     $.ajax({
-  //       type: 'post',
-  //       url: '/session/signin.php',
-  //       data: $('form').serialize(),
-  //       success: function () {
-  //         $("#login-area").empty();
-
-  //         user_name = '<?php session_start(); echo $_SESSION["name"]; ?>';
-  //         console.log(user_name);
-  //         $("#login-area").append("<li class='float-right'><a href='#' onclick='toggleSignIn()'><span class='log-in-text'>" + user_name + "</span><i class='fa fa-sign-out'></i></a></li>");
-  //       }
-  //     });
-  //     e.preventDefault();
-  //   });
-  // });
-
-
-</script> -->
+<div class="clear"></div>
