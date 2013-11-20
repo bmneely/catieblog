5f765281cafc68bfbcd5103cac77579343c6271d
Ben Neely <benjaminneely@u.boisestate.edu>
2013-11-20 16:55:07 -0700

    Signup

--- session/signin.php
+++ session/signin.php
@@ -39,7 +39,7 @@
 	if ($_SESSION["password_error"] === ""){
 		$user = $dao->getUser($email);
 
-    if ($user === null) {
+    if ($user->rowCount() === 0) {
       $_SESSION["form_errors"] = true;
       $_SESSION["email_error"] = "Account not found";
       $_SESSION["email"] = $email;
