1c1eeebb24c9e62d18b5aac7db4aecd942f9ac21
Ben Neely <benjaminneely@u.boisestate.edu>
2013-11-20 14:02:28 -0700

    Working on forms

--- session/signin.php
+++ session/signin.php
@@ -72,12 +72,8 @@
 <html lang="en">
 <head>
   <meta charset="UTF-8">
-  <!-- <meta http-equiv="refresh" content="0; url=http://www.cricketandbea.com/" /> -->
+  <meta http-equiv="refresh" content="0; url=http://www.cricketandbea.com/" />
 </head>
 <body>
-  <h1>  <?php echo "User!!!!"; ?> </h1>
-  <h1>  <?php echo $user; ?> </h1>
-
-  <h1>  <?php echo is_null($user); ?> </h1>
 </body>
 </html>
