aaa586faaf43096f38f10cb92d6104303c8dacf4
Ben Neely <benjaminneely@u.boisestate.edu>
2013-11-20 13:03:20 -0700

    Working on forms

--- session/signin.php
+++ session/signin.php
@@ -13,31 +13,31 @@
     $_SESSION["form_errors"] = true;
     $_SESSION["email_error"] = "An email is required";
   } else {
-    $email = test_input($_POST["email"])
+    $email = test_input($_POST["email"]);
     if (!preg_match("/([\w\-]+\@[\w\-]+\.[\w\-]+)/",$email)) {
       $_SESSION["form_errors"] = true;
       $_SESSION["email_error"] = "Your email is invalid";
     } 
   }
 
-  // if(empty($_POST["pasword"])) {
-  //   $_SESSION["form_errors"] = true;
-  //   $_SESSION["password_error"] = "A password is required";
-  // } else {
-  //   $password = $pass->crypt_pass($_POST['password']);
-  // }
+  if(empty($_POST["pasword"])) {
+    $_SESSION["form_errors"] = true;
+    $_SESSION["password_error"] = "A password is required";
+  } else {
+    $password = $pass->crypt_pass($_POST['password']);
+  }
 
 
-	// if (!isset($_SESSION['form_errors'])){
-	// 	$user = $dao->getUser($email);
+	if (!isset($_SESSION['form_errors'])){
+		$user = $dao->getUser($email);
     
- //    if($pass->is_password_correct($user, $password)){
-	// 		$_SESSION["name"] = user_name($user);
-	// 	} else {
- //      $_SESSION["form_errors"] = true;
- //      $_SESSION["password_error"] = "Invalid password";
- //    }
-	// }
+    if($pass->is_password_correct($user, $password)){
+			$_SESSION["name"] = user_name($user);
+		} else {
+      $_SESSION["form_errors"] = true;
+      $_SESSION["password_error"] = "Invalid password";
+    }
+	}
 
   function user_name($user) {
   	$user_name = "";
