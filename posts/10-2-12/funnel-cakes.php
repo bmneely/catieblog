6cd148bfd73e94e0c592c74ec1f451b5900da8a2
Ben Neely <benjaminneely@u.boisestate.edu>
2013-11-20 21:54:42 -0700

    Comment form

--- posts/10-2-12/funnel-cakes.php
+++ posts/10-2-12/funnel-cakes.php
@@ -35,9 +35,9 @@
             <p>Looks yummy, eh?  I know, not so much.  Turns out sometimes gluten really IS good for something, like keeping funnel cakes together in hot oil.  But, trust me, it really did taste delicious!</p>
             <p>I also started a quilt today!  And if all goes well, I will share pictures soon.  For now, there are enchiladas cooling on the counter and I am HUNGRY!</p>
           
-          <?php include("../../partials/comments.php") ?>
           </div>
         </div>
+        <?php include("../../partials/comments.php") ?>
       </div>
     <?php include("../../partials/footer.php") ?>
     </div>
