ed92a5cb7f0c70c7f2e4afe22210587182b19d46
Ben Neely <benjaminneely@u.boisestate.edu>
2013-11-20 23:43:11 -0700

    Comment form

--- partials/comments.php
+++ partials/comments.php
@@ -6,16 +6,17 @@
 
   <?php
     require_once $_SERVER['DOCUMENT_ROOT'] . "/resources/Dao.php";    
+    $dao = new Dao();
     $comments = $dao->getComments($post_id);
     echo var_dump($comments);
 
-    // foreach ($comments as $comment) {
-    //   echo "<div class='comment-content'>";
-    //   echo $comment["content"];
-    //   echo "<div class='comment-user'>";
-    //   echo $comment["user_id"];
-    //   echo "</div></div>";
-    // }
+    foreach ($comments as $comment) {
+      echo "<div class='comment-content'>";
+      echo $comment["content"];
+      echo "<div class='comment-user'>";
+      echo $comment["user_id"];
+      echo "</div></div>";
+    }
   ?>
 </div>
 
