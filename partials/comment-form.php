0aabf93810937b53db9f44f5570ff17ccb4576d4
Ben Neely <benjaminneely@u.boisestate.edu>
2013-11-20 22:05:48 -0700

    Comment form

--- partials/comment-form.php
+++ partials/comment-form.php
@@ -1,4 +1,5 @@
 <div class="comment-form">
+  <br>TTTTTTTTTTTTTTTTTTTTTT
   <form class="sign-up-form" action="comment-form.php" method="post">
     <label><input type="text" name="content" /></label><br/>/label>
     <input class="submit" type="submit" value="Sign up" />
