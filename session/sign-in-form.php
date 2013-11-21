8c49b893c15a84c9ae2c4cb42646840dea0e28ac
Ben Neely <benjaminneely@u.boisestate.edu>
2013-11-20 23:23:00 -0700

    Comment form

--- session/sign-in-form.php
+++ session/sign-in-form.php
@@ -2,7 +2,7 @@
   <div>
     <a class="close-icon" href="#" onclick="toggleSignIn()">&#10006;</a>
     <br>
-    <form class="sign-in-form" action="../session/signin.php" method="post">
+    <form class="sign-in-form" action="<?php echo $_SERVER['DOCUMENT_ROOT']; echo '/session/signin.php' ?>"  method="post">
       <label><div>Email</div><input type="text" name="email" value= "<?php if(isset($_SESSION['email'])){ echo $_SESSION['email'];}?>" /></label><br/>
       <div class="error"><?php echo $_SESSION["email_error"]; ?></div>
       <label><div>Password</div><input type="password" name="password" /></label>
