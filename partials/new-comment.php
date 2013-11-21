058f83e740d0d79c6dd32e0245dfd37231009ad7
Ben Neely <benjaminneely@u.boisestate.edu>
2013-11-20 23:56:38 -0700

    Comment form

--- partials/new-comment.php
+++ partials/new-comment.php
@@ -1,3 +1,28 @@
 <?php
+  session_start();
   require_once $_SERVER['DOCUMENT_ROOT'] . "/resources/Dao.php";
   $dao = new Dao();
+
+  $comment_date = new DateTime('now');
+  $user_id = $dao->getUser($_SESSION["email"])["id"];
+  $content = clean_input($_POST["content"]);
+
+
+  $dao->saveComment ($comment_date, $user_id, $post_id, $content)
+
+  function clean_input($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+  }
+?>
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="refresh" content="0; url=<?php echo $_SERVER['HTTP_REFERER']; ?>" />
+</head>
+<body>
+</body>
+</html>
