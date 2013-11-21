a2b56cdb482c2c177b6412fb5df4cdd20e3b756e
Ben Neely <benjaminneely@u.boisestate.edu>
2013-11-20 23:49:52 -0700

    Comment form

--- partials/comment-form.php
+++ partials/comment-form.php
@@ -1,5 +1,5 @@
 <div class="comment-form">
-  <form class="commment-form" action="new-comment.php" method="post">
+  <form class="commment-form" action="/new-comment.php" method="post">
     <textarea class="text-area" rows="4" cols="50" name="comment" form="comment-form"></textarea>
     <input class="submit" type="submit" value="Submit Comment" />
   </form>
