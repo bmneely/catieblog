1f4c969b8e10fa7da840336b30363fabdae01e7a
Ben Neely <benjaminneely@u.boisestate.edu>
2013-11-20 23:25:14 -0700

    Comment form

--- session/sign-up-form.php
+++ session/sign-up-form.php
@@ -1,7 +1,7 @@
 <div class="sign-up" id="<?php if(isset($_SESSION['sign_up_form_errors']) && $_SESSION['sign_up_form_errors']) { echo 'show-me';} ?>">
   <a class="close-icon" href="#" onclick="toggleSignUp()">&#10006;</a>
   <br>
-  <form class="sign-up-form" action="<?php echo $_SERVER['DOCUMENT_ROOT']; echo '/session/signup.php' ?>" method="post">
+  <form class="sign-up-form" action="/session/signup.php" method="post">
     <label><span class="sign-up-label">First Name</span><input type="text" name="first" value= "<?php if(isset($_SESSION['email'])){ echo $_SESSION['first_name'];}?>" /></label><br/>
     <div class="error"><?php echo $_SESSION["first_name_error"]; ?></div>
 
