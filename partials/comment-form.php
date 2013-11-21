a4891a6179d9efe7ef75c1b517f35c2dff15db01
Ben Neely <benjaminneely@u.boisestate.edu>
2013-11-21 02:05:07 -0700

    Signup

--- partials/comment-form.php
+++ partials/comment-form.php
@@ -2,6 +2,6 @@
   <form class="commment-form" action="/partials/new-comment.php" method="post">
     <textarea class="text-area" rows="4" cols="50" name="content"></textarea>
     <input type="hidden" name="post_id" value="<?php echo $post_id ?>">
-    <input class="submit" type="submit" value="Submit Comment" />
+    <input class="submit-commit" type="submit" value="Submit Comment" />
   </form>
 </div>
