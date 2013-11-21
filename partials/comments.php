31297ede9d7fb2273cb80a01aa565f551072948e
Ben Neely <benjaminneely@u.boisestate.edu>
2013-11-20 22:58:47 -0700

    Comment form

--- partials/comments.php
+++ partials/comments.php
@@ -2,23 +2,25 @@
   <h2>Comments</h2>
 
 
-  I am right here!
+  I am right here!<br>
 
   <?php
-    echo "test1";
+    echo "test1\n";
 
     require_once "../resources/Dao.php";
     
-    echo "test2";
+    echo "test2\n";
 
     $dao = new Dao();
 
-    echo "test3";
-
+    echo "test3\n";
 
     echo var_dump($dao);
-    echo "string";
+    
+    echo "test4\n";
+    
     $comments = $dao->getComments($post_id);
+
     // echo var_dump($comments);
     // echo $comments;
   ?>
