992d3073349e194db6d8b0ae83f9fff0ab71d0d0
Ben Neely <benjaminneely@u.boisestate.edu>
2013-12-08 11:04:37 -0700

    Add icon

--- partials/comments.php
+++ partials/comments.php
@@ -12,7 +12,7 @@
       echo "<div class='comment-content'>";
       echo htmlspecialchars($comment["content"]);;
       if ($user["email"] == $_SESSION["email"]){
-        echo "<i class='fa fa-trash-o'></i>";
+        echo "<div><i class='fa fa-trash-o'></i></div>";
       }
       echo "<div class='comment-user'>";
       echo htmlspecialchars(user_name($user));
