acb2f571dbdd195743e786e09a6fc6041b741992
Ben Neely <benjaminneely@u.boisestate.edu>
2013-12-09 16:36:35 -0700

    Shopping cart

--- store/shopping_cart.php
+++ store/shopping_cart.php
@@ -1,6 +1,6 @@
 <div class="shopping-cart" >
   <?php if(isset($_SESSION["name"])){
-    echo "<div>Log in to view and add items to your cart</div>"
+    echo "<div>Log in to view and add items to your cart</div>";
   } else {
 
   }
