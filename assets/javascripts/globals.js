057696e09cfdff517934297c820a845398580316
Ben Neely <benjaminneely@u.boisestate.edu>
2013-12-08 18:53:43 -0700

    Add icon

--- assets/javascripts/globals.js
+++ assets/javascripts/globals.js
@@ -27,44 +27,17 @@
   });
 
 
-  $(document).ready(function(){
-    $("#comment-form").submit(function(e){
-      var postData = $(this).serializeArray();
-      console.log(postData);
-      var formURL = $(this).attr("action");
-          $.ajax({
-              type: "POST",
-              url: "/partials/new-comment.php",
-              data: postData,
-              success:function(data, textStatus, jqXHR){
-                $(".comments").append("<div class='comment-content' id='comment-" + postData[1].value + "'>"
-                  + postData[0].value
-                  + "<div class='float-right'><a href='javascript:void(0)' class='delete_comment' value='" + postData[0].value + "'><i class='fa fa-trash-o'></i></a></div>"
-                  + "<div class='comment-user'>"
-                  + postData[2].value
-                  + "</div></div>");
-                alert("ALERT ALERT IT IS THE DOCTOR!!!!");
-              }
-          });
-          e.preventDefault(); //STOP default action
-          // $("#comment-form").submit(); //Submit  the FORM
-      });
-  });
-
-  // $(function () {
-  //   $('form').on('submit', function (e) {
-  //     $.ajax({
-  //       type: 'post',
-  //       url: '/session/signin.php',
-  //       data: $('form').serialize(),
-  //       success: function () {
-  //         $("#login-area").empty();
-
-  //         user_name = '<?php session_start(); echo $_SESSION["name"]; ?>';
-  //         console.log(user_name);
-  //         $("#login-area").append("<li class='float-right'><a href='#' onclick='toggleSignIn()'><span class='log-in-text'>" + user_name + "</span><i class='fa fa-sign-out'></i></a></li>");
-  //       }
-  //     });
-  //     e.preventDefault();
-  //   });
-  // });
+$(document).ready(function(){
+  $("#comment-form").submit(function(e){
+    var postData = $(this).serializeArray();
+    var formURL = $(this).attr("action");
+    $.ajax({
+      type: "POST",
+      url: "/partials/new-comment.php",
+      data: postData,
+      success:function(data, textStatus, jqXHR){
+        $(".comments").append("<div class='comment-content' id='comment-" + encodeURI(postData[1].value) + "'>" + encodeURI(postData[0].value) + "<div class='float-right'><a href='javascript:void(0)' class='delete_comment' value='" + encodeURI(postData[0].value) + "'><i class='fa fa-trash-o'></i></a></div>" + "<div class='comment-user'>" + encodeURI(postData[2].value) + "</div></div>");
+      }
+    });
+  e.preventDefault();
+});
