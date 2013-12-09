417230ba2bed7b269ae6cde87a2b5c5c38172ef4
Ben Neely <benjaminneely@u.boisestate.edu>
2013-12-09 15:30:50 -0700

    Prevent bad stuff

--- partials/comment-form.php
+++ partials/comment-form.php
@@ -1,12 +1,8 @@
-<?php 
-  $_SESSION["secret_number"] = uniqid();
-?>
 <div class="comment-form">
   <form class="commment-form" id="comment-form" action="/partials/new-comment.php" method="post">
     <textarea class="text-area" id="comment-text-area" rows="4" cols="50" name="content"></textarea>
     <input type="hidden" name="post_id" value="<?php echo $post_id ?>">
     <input type="hidden" name="user_name" value="<?php echo $usr->user_name($user) ?>">
-    <input type="hidden" name="secret_number" value="<?php echo $_SESSION["secret_number"] ?>">
     <input class="submit-commit" type="submit" value="Submit Comment" />
   </form>
 </div>
--- partials/new-comment.php
+++ partials/new-comment.php
@@ -1,24 +1,21 @@
 <?php
-
   session_start();
-  if ($_SESSION["secret_number"] == $_POST("secret_number")){
-    require_once $_SERVER['DOCUMENT_ROOT'] . "/resources/Dao.php";
-    $dao = new Dao();
+  require_once $_SERVER['DOCUMENT_ROOT'] . "/resources/Dao.php";
+  $dao = new Dao();
 
-    $comment_date = new DateTime("now", new DateTimeZone('America/Denver')); 
-    $user = $dao->getUser($_SESSION["email"]);
-    $user_id = $user["id"];
-    $content = clean_input($_POST["content"]);
-    $post_id = $_POST["post_id"];
+  $comment_date = new DateTime("now", new DateTimeZone('America/Denver')); 
+  $user = $dao->getUser($_SESSION["email"]);
+  $user_id = $user["id"];
+  $content = clean_input($_POST["content"]);
+  $post_id = $_POST["post_id"];
 
-    $id = $dao->saveComment ($comment_date, $user_id, $post_id, $content);
+  $id = $dao->saveComment ($comment_date, $user_id, $post_id, $content);
 
-    function clean_input($data) {
-      $data = trim($data);
-      $data = stripslashes($data);
-      $data = htmlspecialchars($data);
-      return $data;
-    }
-    echo $id; 
+  function clean_input($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
   }
+  echo $id;
 ?>
