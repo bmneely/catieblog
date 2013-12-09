304f03f3755ea64971e4a5e49726eb5366049ab4
Ben Neely <benjaminneely@u.boisestate.edu>
2013-12-08 18:57:57 -0700

    Add icon

--- assets/javascripts/globals.js
+++ assets/javascripts/globals.js
@@ -27,17 +27,24 @@
   });
 
 
-$(document).ready(function(){
-  $("#comment-form").submit(function(e){
-    var postData = $(this).serializeArray();
-    var formURL = $(this).attr("action");
-    $.ajax({
-      type: "POST",
-      url: "/partials/new-comment.php",
-      data: postData,
-      success:function(data, textStatus, jqXHR){
-        $(".comments").append("<div class='comment-content' id='comment-" + postData[1].value + "'>" + postData[0].value + "<div class='float-right'><a href='javascript:void(0)' class='delete_comment' value='" + postData[0].value + "'><i class='fa fa-trash-o'></i></a></div>" + "<div class='comment-user'>" + postData[2].value + "</div></div>");
-      }
-    });
-  e.preventDefault();
-});
+  $(document).ready(function(){
+    $("#comment-form").submit(function(e){
+      var postData = $(this).serializeArray();
+      var formURL = $(this).attr("action");
+          $.ajax({
+              type: "POST",
+              url: "/partials/new-comment.php",
+              data: postData,
+              success:function(data, textStatus, jqXHR){
+                $(".comments").append("<div class='comment-content' id='comment-" + postData[1].value + "'>"
+                  + postData[0].value
+                  + "<div class='float-right'><a href='javascript:void(0)' class='delete_comment' value='" + postData[0].value + "'><i class='fa fa-trash-o'></i></a></div>"
+                  + "<div class='comment-user'>"
+                  + postData[2].value
+                  + "</div></div>");
+                alert("ALERT ALERT IT IS THE DOCTOR!!!!");
+              }
+          });
+          e.preventDefault();
+  });
+
