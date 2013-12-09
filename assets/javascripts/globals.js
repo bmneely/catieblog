dc9b43b2b9c352cb85efef406680a7e8b4173617
Ben Neely <benjaminneely@u.boisestate.edu>
2013-12-08 18:59:11 -0700

    Add icon

--- assets/javascripts/globals.js
+++ assets/javascripts/globals.js
@@ -30,6 +30,7 @@
   $(document).ready(function(){
     $("#comment-form").submit(function(e){
       var postData = $(this).serializeArray();
+      console.log(postData);
       var formURL = $(this).attr("action");
           $.ajax({
               type: "POST",
@@ -37,7 +38,7 @@
               data: postData,
               success:function(data, textStatus, jqXHR){
                 $(".comments").append("<div class='comment-content' id='comment-" + postData[1].value + "'>"
-                  + postData[0].value
+                  + HtmlEncode(postData[0].value)
                   + "<div class='float-right'><a href='javascript:void(0)' class='delete_comment' value='" + postData[0].value + "'><i class='fa fa-trash-o'></i></a></div>"
                   + "<div class='comment-user'>"
                   + postData[2].value
@@ -45,6 +46,25 @@
                 alert("ALERT ALERT IT IS THE DOCTOR!!!!");
               }
           });
-          e.preventDefault();
+          e.preventDefault(); //STOP default action
+          // $("#comment-form").submit(); //Submit  the FORM
+      });
   });
 
+  // $(function () {
+  //   $('form').on('submit', function (e) {
+  //     $.ajax({
+  //       type: 'post',
+  //       url: '/session/signin.php',
+  //       data: $('form').serialize(),
+  //       success: function () {
+  //         $("#login-area").empty();
+
+  //         user_name = '<?php session_start(); echo $_SESSION["name"]; ?>';
+  //         console.log(user_name);
+  //         $("#login-area").append("<li class='float-right'><a href='#' onclick='toggleSignIn()'><span class='log-in-text'>" + user_name + "</span><i class='fa fa-sign-out'></i></a></li>");
+  //       }
+  //     });
+  //     e.preventDefault();
+  //   });
+  // });
