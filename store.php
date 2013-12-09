e9a03b32bc92c82863fce8afa68fa3d20c7bbbcf
Ben Neely <benjaminneely@u.boisestate.edu>
2013-12-09 16:52:39 -0700

    Shopping cart

--- store.php
+++ store.php
@@ -12,8 +12,8 @@
     <?php include("partials/banner.php") ?>
 
     <div id="body">
-      <?php include("store/shopping_cart.php") ?>
-      
+      <?php# include("store/shopping_cart.php") ?>
+
       <div class="inventory">
         <?php $image_location = "posts/2-3-13/photos/hanger05.jpg"; $item_name = "Crochet hanger covers"; $price = "\$8"; $item_desc = "Beautiful hand croched hanger covers."; include("store/item_partial.php") ?>
         <?php $image_location = "posts/1-27-13/photos/quill-01.jpg"; $item_name = "Cat and Bunny Dolls"; $price = "\$15"; $item_desc = "Hand made paper mache dolls. Includes brown cat and white bunny."; include("store/item_partial.php") ?>
--- store/shopping_cart.php
+++ store/shopping_cart.php
@@ -1,13 +1,10 @@
-// <?php
-//   require
-// ?>
-// <div class="shopping-cart-container" >
-//   <?php if(isset($_SESSION["name"])){
-//   } else {
-//     echo "<div><h3>Log in to view and add items to your cart<h3></div>";
-//   }
-//     $dao = new Dao();
-//     echo "<div><h3>Current Items</h3></div>"
+<div class="shopping-cart-container" >
+  <?php if(isset($_SESSION["name"])){
+  } else {
+    echo "<div><h3>Log in to view and add items to your cart<h3></div>";
+  }
+    $dao = new Dao();
+    echo "<div><h3>Current Items</h3></div>"
 
-//   ?>
-// </div>
+  ?>
+</div>
