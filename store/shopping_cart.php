3ad0979455d0c63a650b9af10b9c384fe8688dfc
Ben Neely <benjaminneely@u.boisestate.edu>
2013-12-09 16:51:15 -0700

    Shopping cart

--- store/shopping_cart.php
+++ store/shopping_cart.php
@@ -1,7 +1,13 @@
-<div class="shopping-cart-container" >
-  <?php if(isset($_SESSION["name"])){
-  } else {
-    echo "<div>Log in to view and add items to your cart</div>";
-  }
-  ?>
-</div>
+// <?php
+//   require
+// ?>
+// <div class="shopping-cart-container" >
+//   <?php if(isset($_SESSION["name"])){
+//   } else {
+//     echo "<div><h3>Log in to view and add items to your cart<h3></div>";
+//   }
+//     $dao = new Dao();
+//     echo "<div><h3>Current Items</h3></div>"
+
+//   ?>
+// </div>
